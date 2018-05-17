2b2c7490ce4049aa3926268284912cb0f8987dc3
kormos3 <kormos3@lab101-10.iit.uni-miskolc.hu>
2018-05-17 13:54:38 +0200

    Kenyer-Aru

---
+++ 04.09_gyak/src/myproducts/futo/Futtathato.java
@@ -0,0 +1,15 @@
+package myproducts.futo;
+
+import myproducts.*;
+
+public class Futtathato {
+
+
+	public static void main(String[] args) {
+		Aru	aru_objektum = new Aru("aru", 20, 27);
+		Kenyer kenyer_objektum = new Kenyer("kenyer", 100, 15, 0.75);
+	}
+	
+	
+
+}
